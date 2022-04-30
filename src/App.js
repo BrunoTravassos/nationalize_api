f3a14e71cfdf5256a5c767b4dec01fb9f6ca300d
BrunoTravassos <brunodefrontin@gmail.com>
2022-04-30 15:33:55 -0300

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import paises from "./paises";
+const api = {
+  baseURL: `https://api.nationalize.io`,
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const list = document.getElementById("location-box");
+
+  const search = (src) => {
+    axios.get(`${api.baseURL}?name=${query}`).then((response) => {
+      const arr = response.data.country;
+      for (var i = 0; i < arr.length; i++) {
+        const name = response.data.country[i].country_id;
+        const prob = response.data.country[i].probability;
+
+        for (var x = 0; x < paises.length; x++) {
+          if (paises[x].code === name) {
+            displayResults(paises[x].name, prob.toFixed(3));
+          }
+        }
+      }
+    });
+
+    function displayResults(name, prob) {
+      let li = document.createElement("li");
+      li.innerText = name + " - " + prob+" %";
+      list.appendChild(li);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          textalign: "center",
+        }}
+        className="search-box"
+      >
+        <h1>
+          Origem do Nome
+        </h1>
+        <form>
+          <input
+            type="text"
+            id="search-box"
+            className="search-bar"
+            placeholder="Digite um Nome..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+          />
+          <input
+            type="button"
+            placeholder="name"
+            onClick={search}
+            value="Pesquisar"
+          />
+        </form>
+
+        <ul style={{ listStyleType: "none" }} id="location-box"></ul>
+      </div>
+    </>
   );
 }
 
